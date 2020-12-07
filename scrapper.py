b98412d03f19560f4cfb223d394e71876586a04f
Nirvighan <62175651+Nirvighan@users.noreply.github.com>
2020-12-07 18:48:55 +0530

    Add files via upload

---
+++ scrapper.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+import time 
+import csv
+import requests
+import pandas as pd
+
+# OPEN THE URL
+start_url = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+
+# OPEN THE LINK WITH THE BROWSER
+#browser = webdriver.Chrome(r"C:\Users\Nirvighan\Downloads\chromedriver_win32\chromedriver")
+#browser.get(start_url)
+#time.sleep(10)
+
+page = requests.get(start_url)
+soup = bs(page.text,'html.parser')
+star_table = soup.find('table')
+
+# FUNTION FOR ADDING AND SCRAPPING THE DATA
+
+temp_list =[]
+
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td ]
+    temp_list.append(row)
+
+
+
+print(temp_list)
+
+star_names = []
+star_distance = []
+star_mass = []
+star_radius = []
+star_lum = []
+
+for i in range(1,len(temp_list)):
+    star_names.append(temp_list[i][1])
+    star_distance.append(temp_list[i][3])
+    temp_list[i][5] = temp_list[i][5]*0.000954588
+    star_mass.append(temp_list[i][5])
+    #star_radius.append(float(temp_list[i][6])*0.102763)
+    star_lum.append(temp_list[i][7])
+
+df = pd.DataFrame(list(zip(star_names,star_distance,star_mass,star_radius,star_lum)),columns = ['star_names','distance','mass','radius','luminosity'])
+df.to_csv('stars.csv')
+#
+#*
